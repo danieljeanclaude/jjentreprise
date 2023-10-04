4a9d9d7db68fbf23a7b824b53cc7a093fbe66b31
danieljeanclaude <danieljean.claude1315@gmail.com>
2023-10-04 19:53:56 +0530

    MDB Added Layouts Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LayoutsComponent } from './layouts/layouts/layouts.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent,
+    children: [
+      {
+        path: '', component: LayoutsComponent,
+        children: [
+          { path: '', redirectTo: '/home', pathMatch: 'full' },
+          { path: 'home', title: 'Home', component: HomeComponent },
+        ]
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
